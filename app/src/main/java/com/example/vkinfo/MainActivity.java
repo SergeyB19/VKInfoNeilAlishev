91fe59ca77f1d54026081b74dfc50f9daabb6f8e
Bayakan <spring.boot@hotmail.com>
2024-03-24 18:20:05 +0300

    Приложение "Поиск ВКонтакте" (Часть 1). Компонент Button.

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.vkinfo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText searchField;
+    private Button searchButton;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        searchField = findViewById(R.id.et_search_field);
+        searchButton = findViewById(R.id.b_search_vk);
+        result = findViewById(R.id.tv_result);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("Кнопка была нажата!");
+            }
+        };
+        searchButton.setOnClickListener(onClickListener);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
