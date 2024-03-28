9ed2f5540e284e10260693f88694e967e4900fb9
Bayakan <spring.boot@hotmail.com>
2024-03-28 10:06:01 +0300

    AsyncTask (часть 2).

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.vkinfo;
 import static com.example.vkinfo.utils.NetworkUtils.generateURL;
 import static com.example.vkinfo.utils.NetworkUtils.getResponseFromURL;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +18,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.channels.AsynchronousChannel;
 
 public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+
+    class VKQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            result.setText(response);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +58,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 URL generatedURL = generateURL(searchField.getText().toString());
-                String response = null;
-                try {
-                    response = getResponseFromURL(generatedURL);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+
 //                result.setText(generatedURL.toString());
-                result.setText(response);
+                new VKQueryTask().execute(generatedURL);
             }
         };
         searchButton.setOnClickListener(onClickListener);
